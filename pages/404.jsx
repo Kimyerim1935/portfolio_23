2635d3cb44df8b7163279d5b15057834e1b7328c
Kimyerim1935 <kimyerim1935@naver.com>
2023-02-19 15:59:30 +0900

    add: page settings

---
+++ pages/404.jsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Oops! Page is not found!</h1>
+      <h1>Reload Please :)</h1>
+    </div>
+  )
+}
---
+++ pages/projects.tsx
@@ -0,0 +1,5 @@
+import Layout from 'components/layout'
+
+export default function Projects() {
+  return <Layout>this page is about my project</Layout>
+}
