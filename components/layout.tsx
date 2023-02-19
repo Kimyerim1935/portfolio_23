c8b4735f4c4c4e0859acfae0e2910077c899ed46
Kimyerim1935 <kimyerim1935@naver.com>
2023-02-19 18:27:17 +0900

    mod: layout component

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,11 +3,10 @@ import Footer from 'components/footer'
 
 export default function Layout({ children }: any) {
   return (
-    <div>
+    <>
       <Header />
-      <h1>레이아웃</h1>
       {children}
       <Footer />
-    </div>
+    </>
   )
 }
